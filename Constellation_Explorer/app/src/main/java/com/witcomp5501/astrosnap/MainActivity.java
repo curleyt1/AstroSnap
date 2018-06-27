d79eded847056b80a3e8634c0bcadecd60c57841
Ethan DiPilato <dipilatoe@users.noreply.github.com>
2018-06-27 11:17:08 -0400

    finished method to read wikilinks file, working on method to read template data file

--- Constellation_Explorer/app/src/main/java/com/witcomp5501/astrosnap/MainActivity.java
+++ Constellation_Explorer/app/src/main/java/com/witcomp5501/astrosnap/MainActivity.java
@@ -3,6 +3,7 @@ package com.witcomp5501.astrosnap;
 import android.Manifest;
 import android.app.Activity;
 import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
@@ -12,6 +13,7 @@ import android.support.v4.content.ContextCompat;
 import android.widget.Button;
 import java.io.*;
 import java.lang.String;
+import java.util.Scanner;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
@@ -80,6 +82,10 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
         permitCamera();
         Log.d(TAG, "Creating and setting view");
 
+        String[][] wiki = new String [89][2]; //wiki[n][0] = constellation_name  wiki[n][1] = wiki_link
+        String[][][] templateData = new String [5][89][31]; //
+        wiki = readWikiLinks();
+
         setContentView(R.layout.home_screen);
 
         mOpenCvCameraView = (CameraBridgeViewBase) new JavaCameraView(this, -1);
@@ -136,19 +142,54 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
         }
     }
 
+    //this function reads into a 3D array the data from the constellation templates
+    //this will allow for easy searching through the data when looking for a match
     private String[][][] readTemplateData() {
-        String filename="Constellation_Explorer/constellations_database.csv";
-        InputStream read=new FileInputStream(CheckTest.class.getClassLoader().getResourceAsStream(filename));
-        String[][][] data = new String [5][89][31];
+        try {
+            String[][][] data = new String [5][89][31];
+            int numStars; //maybe use this, would have to insert star counts in csv
+            DataInputStream textFileStream = new DataInputStream(getAssets().open(String.format("constellations_database.csv")));
+            Scanner sc = new Scanner(textFileStream);
+            sc.useDelimiter(",");
+            while(sc.hasNext()){
+                for(int constell=0;constell<89;constell++){
+                    data[0][constell][0] = sc.next();
+                    numStars = sc.nextInt();
+                    for(int dataCat=0;dataCat<5;dataCat++){
+                        for(int star=0;star<numStars;star++){
+
+                        }
+                    }
+                }
+                //scanner delimiter use of commas
+                //star will have to be reset after every row
+                //constell should go from 0-88
+                //data cat needs to be reset after each constellation
+            }
+            sc.close();
+            return data;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return data;
     }
 
+    //this function reads into a 2D array the wikipedia links for each of the 88 constellations
     private String[][] readWikiLinks() {
-        String filename="constellations_wiki_links.csv";
-        InputStream read=new FileInputStream(CheckTest.class.getClassLoader().getResourceAsStream(filename));
-        String[][] data = new String [89][2];
-
-        return data;
+        try {
+            String[][] data = new String [89][2];
+            DataInputStream textFileStream = new DataInputStream(getAssets().open(String.format("constellations_wiki_links.txt")));
+            Scanner sc = new Scanner(textFileStream);
+            for(int x=0;x<89;x++){
+                data[x][0] = sc.next();
+                data[x][1] = sc.next();
+            }
+            sc.close();
+            return data;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
